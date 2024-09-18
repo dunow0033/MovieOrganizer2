f630791e631063a39724ba8630434b982691158c
Brian Donnoe <dunow33@hotmail.com>
2024-09-17 23:27:12 -0400

    initial commit

---
+++ MovieOrganizer/Data/MovieDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieOrganizer.Models;
+
+namespace MovieOrganizer.Data
+{
+    public class MovieDBContext : DbContext
+    {
+        public MovieDBContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
